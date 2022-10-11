fdbe3a04388d96dadc5b930f58d93a4a2a41910c
Chris Kung <christopher.w.kung@nasa.gov>
2022-10-11 12:05:10 -0400

    Added test to show that gfortran can call subroutines within an OpenACC parallel region that reference module arrays without passing the module array as a parameter AFTER a subroutine in an OpenACC Parallel region is called with the module array as a parameter.

--- issue1/test.F90
+++ issue1/test.F90
@@ -1,13 +1,39 @@
-program test_array_adjust
+program issue1
     use testMod
     implicit none
 
     call allocate_array()
 !$acc parallel
-    call set_array(test_array)
-    ! call set_array()
+
+    !*****************************************************************
+    ! For gfortran to see a module array in a subroutine running
+    ! within an OpenACC parallel region, the module array (seems like it)
+    ! needs to be passed first into a subroutine that has that module array
+    ! as a parameter.  Subsequent subroutines that run within an OpenACC
+    ! parallel region do not need to pass the array as a parameter.
+    ! Thus, to execute properly with gfortran, the call to 'zero_array' will
+    ! need 'test_array' as a parameter.  Otherwise, it will return the following:
+    ! libgomp: cuStreamSynchronize error: an illegal memory access was encountered
+    !
+    ! nvfortran is able to execute all the subroutines in the OpenACC parallel
+    ! region without having 'test_array' as a parameter.
+    !*****************************************************************
+
+    call zero_array(test_array)
+    ! call zero_array()
+
+    call array_add_one()
+    call array_add_one()
+    call array_add_one()
+    call array_add_one()
+    call array_add_one()
+    call array_add_one()
+    call array_add_one()
+    call array_add_one()
+    call array_add_one()
+    call array_add_one()
+
 !$acc end parallel
     call print_array()
 
-end program test_array_adjust
-
+end program issue1
--- issue1/testMod.F90
+++ issue1/testMod.F90
@@ -4,50 +4,81 @@ module testMod
 
     implicit none
 
-    public set_array
+    public zero_array, array_add_one, allocate_array
 
     double precision, dimension(:), allocatable :: test_array
     integer :: array_size = 1000*1000*900
 
-    interface set_array
-        module procedure set_array_without_param
-        module procedure set_array_with_param
+    interface zero_array
+        module procedure zero_array_without_param
+        module procedure zero_array_with_param
+    end interface
+    interface array_add_one
+        module procedure array_add_one_without_param
+        module procedure array_add_one_with_param
+    end interface
+
+    interface allocate_array
+        module procedure allocate_with_param
+        module procedure allocate_without_param
     end interface
 
 !$acc declare create(array_size, test_array)
 
     contains
 
-    subroutine allocate_array()
+    subroutine allocate_with_param(test_array)
+        double precision, allocatable :: test_array(:)
+        allocate(test_array(array_size))
+    end subroutine
+
+    subroutine allocate_without_param()
         allocate(test_array(array_size))
 #ifdef GNU_OACC
-        call acc_copyin(test_array)
+        call acc_create(test_array)
 #endif
     end subroutine
 
-
-    subroutine set_array_without_param
-    !subroutine set_array
-!$acc routine gang
+    subroutine zero_array_without_param
+        !$acc routine gang
         integer :: ii
-!$acc loop gang vector collapse(1)
+        !$acc loop gang !collapse(1)
         do ii = 1, array_size
-            test_array(ii) = 1.0
+            test_array(ii) = 0.0
+        enddo
+    end subroutine
+
+    subroutine array_add_one_without_param
+        !$acc routine gang
+        integer :: ii
+        !$acc loop gang !collapse(1)
+        do ii = 1, array_size
+            test_array(ii) = test_array(ii) + 1.0
         enddo
 
     end subroutine
 
-    subroutine set_array_with_param(test_array)
+    subroutine zero_array_with_param(test_array)
+        !$acc routine gang
+        integer :: ii
+        double precision :: test_array(:)
+        !$acc loop gang !collapse(1)
+        do ii = 1, array_size
+            test_array(ii) = 0.0
+        enddo
+    end subroutine
+
+    subroutine array_add_one_with_param(test_array)
         !subroutine set_array
     !$acc routine gang
             integer :: ii
-            double precision :: test_array(array_size)
-    !$acc loop gang vector collapse(1)
+            double precision :: test_array(:)
+    !$acc loop gang !collapse(1)
             do ii = 1, array_size
-                test_array(ii) = 1.0
+                test_array(ii) = test_array(ii) + 1.0
             enddo
 
-        end subroutine
+    end subroutine
 
     subroutine print_array()
 
